75bfa5673177f0d3ead01b7f48407a4cff55e453
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-10-26 16:35:50 +0700

    Added login page

---
+++ src/controller/loginController.js
@@ -0,0 +1,7 @@
+const getLoginPage = (req, res) => {
+  return res.render("login.ejs");
+};
+
+module.exports = {
+  getLoginPage,
+};
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,28 +1,30 @@
 import express from "express";
-import homeController from '../controller/homeController';
-import apiController from '../controller/apiController';
+import homeController from "../controller/homeController";
+import apiController from "../controller/apiController";
+import loginController from "../controller/loginController";
 
 const router = express.Router();
 
 /**
- * 
+ *
  * @param {*} app : express app
  */
 
 const initWebRoutes = (app) => {
-    //path, handler
-    router.get("/", homeController.handleHelloWord);
-    router.get("/user", homeController.handleUserPage);
-    router.post("/users/create-user", homeController.handleCreateNewUser);
-    router.post("/delete-user/:id", homeController.handleDelteUser)
-    router.get("/update-user/:id", homeController.getUpdateUserPage);
-    router.post("/user/update-user", homeController.handleUpdateUser);
+  //path, handler
+  router.get("/", homeController.handleHelloWord);
+  router.get("/user", homeController.handleUserPage);
+  router.post("/users/create-user", homeController.handleCreateNewUser);
+  router.post("/delete-user/:id", homeController.handleDelteUser);
+  router.get("/update-user/:id", homeController.getUpdateUserPage);
+  router.post("/user/update-user", homeController.handleUpdateUser);
 
-    //rest api
-    //GET - R, POST- C, PUT - U, DELETE - D
-    router.get("/api/test-api", apiController.testApi);
+  //rest api
+  //GET - R, POST- C, PUT - U, DELETE - D
+  router.get("/api/test-api", apiController.testApi);
+  router.get("/login", loginController.getLoginPage);
 
-    return app.use("/", router);
-}
+  return app.use("/", router);
+};
 
 export default initWebRoutes;
